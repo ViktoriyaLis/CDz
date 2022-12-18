4949b622c8ad9e74433684e7230a556a777f8e19
Viktoriya <vika2113@mail.ru>
2022-12-18 14:37:48 +0900

    Четвертая задача три переменные

---
+++ Zchetyry/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напис. программу, которая принимает 3 числа и выдает максимальное из них
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int c = int.Parse (Console.ReadLine()!);
+int max = a;
+if (a>max)
+{
+    max=a;
+}
+
+if (b>max) 
+{
+    max = b;
+}
+
+if (c>max) 
+{
+    max = c;
+}
+Console.WriteLine(max);
