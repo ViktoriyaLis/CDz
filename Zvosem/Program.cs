773c91dceae7bc7337d7121bbdd1aa9e812224cf
Viktoriya <vika2113@mail.ru>
2022-12-18 15:20:14 +0900

    шаблон задач на доработку

---
+++ Zvosem/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напис прогр, которая принимает число N,на выходе показывает все четные числа от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+N = a;
+int b = 1;
+int c = 0;
+
+while (a<=N)
+if (a%=c)
+{
+    Console.WriteLine(b, a + " ");
+}
+else a = a+1;
