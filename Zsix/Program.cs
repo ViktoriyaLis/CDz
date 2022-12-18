274ac0e92aa65ce9ac7270a8b3b639a365ae3d04
Viktoriya <vika2113@mail.ru>
2022-12-18 15:30:26 +0900

    задача подредакт

---
+++ Zsix/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напис прогр, которая принимает число и выдает является ли оно четным
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+int b = 0;
+if (a%2==b)
+{
+    Console.WriteLine($" {a} - четное число");
+}
+if (a%2!=b)
+{
+    Console.WriteLine($" {a} - нечетное число");
+}
