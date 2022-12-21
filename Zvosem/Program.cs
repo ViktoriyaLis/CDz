2a21af5d7dd86de6735049fb9af7b931c84f45a6
Viktoriya <vika2113@mail.ru>
2022-12-21 22:12:32 +0900

    Алгоритм не работает

--- Zvosem/Program.cs
+++ Zvosem/Program.cs
@@ -1,19 +1,17 @@
 ﻿// Напис прогр, которая принимает число N,на выходе показывает все четные числа от 1 до N
 
-// К сожалению не разобралась, чтобы и цифра 1 отображалась по итогу
-
+// К сожалению не разобралась c этой задачей
 Console.Clear();
 
 Console.WriteLine("Введите число");
 int N = int.Parse(Console.ReadLine()!);
 int a = 2;
 int StartNumber = 1;
-if (N > 1)
-{
-    while (a<=N)
-    {
-    Console.WriteLine( StartNumber + a + " ");
-    a = a+2;
-    }
-}
+while (a<=N)
+        if (a%2==0)
+        {
+         Console.WriteLine( StartNumber +  " ");
+        a = a+1;
+        }
+else a++;
 
