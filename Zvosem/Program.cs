b18d8e7469e3096f7331f88d0c84ba3f25c86f91
Viktoriya <vika2113@mail.ru>
2022-12-21 22:01:02 +0900

    Доработка восьмой задачи

--- Zvosem/Program.cs
+++ Zvosem/Program.cs
@@ -1,16 +1,19 @@
 ﻿// Напис прогр, которая принимает число N,на выходе показывает все четные числа от 1 до N
 
+// К сожалению не разобралась, чтобы и цифра 1 отображалась по итогу
+
 Console.Clear();
 
 Console.WriteLine("Введите число");
 int N = int.Parse(Console.ReadLine()!);
-N = a;
-int b = 1;
-int c = 0;
-
-while (a<=N)
-if (a%=c)
+int a = 2;
+int StartNumber = 1;
+if (N > 1)
 {
-    Console.WriteLine(b, a + " ");
+    while (a<=N)
+    {
+    Console.WriteLine( StartNumber + a + " ");
+    a = a+2;
+    }
 }
-else a = a+1;
+
