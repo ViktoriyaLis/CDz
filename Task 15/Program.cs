f130659359ce189162c4ac77f22a32e4b9a5da73
Viktoriya <vika2113@mail.ru>
2022-12-23 23:47:15 +0900

    Является день выходным

---
+++ Task 15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Нап прогрм, принимает на вход цифру, обознач день недели, проверяет,является ли выходным
+
+Console.WriteLine("Введите цифру от 1 до 7");
+int number = int.Parse (Console.ReadLine()!);
+if (number>0 && number<=7)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Да, сегодня выходной");
+    }
+    else Console.WriteLine("Нет, сегодня рабочий день");
+}
+else Console.WriteLine("Ошибка. Введите цифру от 1 до 7");
