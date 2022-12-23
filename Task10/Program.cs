7ec0aa0e2ef28ec10e8ff050a185a58761dd1319
Viktoriya <vika2113@mail.ru>
2022-12-23 23:27:46 +0900

    Вторую цифру трехзн числа

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Нап программу, принимает на вход трехзн число, на выходе показывает вторую цифру числа
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+int FindDigit(int number)
+{
+    int digit1 = number / 10;
+    int digit2 = digit1 % 10;
+    return digit2;
+}
+int SecondDigit = FindDigit(number);
+Console.WriteLine($"В числе {number} второй цифрой является {SecondDigit}");
