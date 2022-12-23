820e81dde6b8641566f920c7543374d0cd4ebc74
Viktoriya <vika2113@mail.ru>
2022-12-24 00:06:18 +0900

    надо переделать с прой остатка

---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Нап прог, выводи третью цифру заданного числа, или сообщает, что ее нет
+
+Console.WriteLine("Введите число");
+int number = int.Parse (Console.ReadLine()!);
+if (number > 100)
+{   int FindDigit(int number)
+    {
+    int digit1 = number / 100;
+    
+    return digit1;
+    }
+    int ThreeDigit = FindDigit(number);
+    Console.WriteLine($"Третьей цифрой в числе {number} является {ThreeDigit}");
+}
+else Console.WriteLine("Третьей цифры нет");
