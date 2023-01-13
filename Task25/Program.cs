9f99497d6696e7a387ae6d676e7a8ffcc75b3095
Viktoriya <vika2113@mail.ru>
2023-01-13 22:49:43 +0900

    Возведение в степень с формулой

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/ 
+int getUserValue (string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int numberA = getUserValue("Введите первое число");
+int numberB = getUserValue("Введите второе число");
+Console.Write(Math.Pow(numberA,numberB));
+
+
