3f90d193c9918cebfb9d070bfff56b6b4a975b4c
Viktoriya <vika2113@mail.ru>
2023-01-23 23:11:22 +0900

    Тест сохранения

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.0, 7, 8, -2, -2 -> 2; 1, -7, 567, 89, 223-> 3
-int getUserData (string message)
+int [] getUserData (string message)
 {
     Console.WriteLine(message);
-    int UserData = int.Parse(Console.ReadLine()!);
+    int [] UserData = array[](Console.ReadLine()!);
     return UserData;
 }
+int [] numbers = getUserData ("Введите числа");
