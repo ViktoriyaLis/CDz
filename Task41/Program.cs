09c4af296fccb75a59bcf94d952c9fd4842df7c2
Viktoriya <vika2113@mail.ru>
2023-01-23 23:26:38 +0900

    Кол-во цифр преобразуем в массив и выдаем кол-во положит

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,8 +1,39 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.0, 7, 8, -2, -2 -> 2; 1, -7, 567, 89, 223-> 3
-int [] getUserData (string message)
+/*
+Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int getUserData (string message)
 {
     Console.WriteLine(message);
-    int [] UserData = array[](Console.ReadLine()!);
+    int UserData = int.Parse(Console.ReadLine()!);
     return UserData;
 }
-int [] numbers = getUserData ("Введите числа");
+
+int lenght = getUserData("Введите количество чисел, которое хотите ввести: ");
+int [] numbers = new int [lenght];
+
+for (int i = 0; i < lenght; i++)
+{
+    Console.WriteLine("Введите число: ");
+    numbers [i] = int.Parse(Console.ReadLine()!);
+}
+
+int PositiveNumbers(int [] array)
+{
+    int lenght = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > 0)
+        {
+            lenght++;
+        }
+    }
+    return lenght;
+}
+
+int positiveNumbersLenght = PositiveNumbers(numbers);
+Console.WriteLine($"Количество чисел больше 0 равно {positiveNumbersLenght}");
